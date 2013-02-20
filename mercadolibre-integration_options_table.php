db6a058fec28742409e0d554ff92edc0f7dc04d5
hernanjh@gmail.com <hernanjh@gmail.com@838874cd-0256-d14d-98fd-3a6a1e5cbc64>
2013-02-20 06:14:16 +0000

    new

---
+++ mercadolibre-integration_options_table.php
@@ -0,0 +1,308 @@
+<?php
+/*
+Plugin Name: Custom List Table Example
+Plugin URI: http://www.mattvanandel.com/
+Description: A highly documented plugin that demonstrates how to create custom List Tables using official WordPress APIs.
+Version: 1.1
+Author: Matt Van Andel
+Author URI: http://www.mattvanandel.com
+License: GPL2
+*/
+/*  Copyright 2011  Matthew Van Andel  (email : matt@mattvanandel.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+/*************************** LOAD THE BASE CLASS *******************************
+ *******************************************************************************
+ * The WP_List_Table class isn't automatically available to plugins, so we need
+ * to check if it's available and load it if necessary.
+ */
+if(!class_exists('WP_List_Table')){
+    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+
+
+
+
+/************************** CREATE A PACKAGE CLASS *****************************
+ *******************************************************************************
+ * Create a new list table package that extends the core WP_List_Table class.
+ * WP_List_Table contains most of the framework for generating the table, but we
+ * need to define and override some methods so that our data can be displayed
+ * exactly the way we need it to be.
+ * 
+ * To display this example on a page, you will first need to instantiate the class,
+ * then call $yourInstance->prepare_items() to handle any data manipulation, then
+ * finally call $yourInstance->display() to render the table to the page.
+ * 
+ * Our theme for this list table is going to be movies.
+ */
+class TT_MLInt_List_Table extends WP_List_Table {
+    
+    /** ************************************************************************
+     * Normally we would be querying data from a database and manipulating that
+     * for use in your list table. For this example, we're going to simplify it
+     * slightly and create a pre-built array. Think of this as the data that might
+     * be returned by $wpdb->query().
+     * 
+     * @var array 
+     **************************************************************************/
+	 
+
+    
+    /** ************************************************************************
+     * REQUIRED. Set up a constructor that references the parent constructor. We 
+     * use the parent reference to set some default configs.
+     ***************************************************************************/
+	var $options = array();
+    function __construct($optionsparam){
+        global $status, $page;
+		$this->options = $optionsparam;
+        //Set parent defaults
+        parent::__construct( array(
+            'singular'  => 'id',     //singular name of the listed records
+            'plural'    => 'ids',    //plural name of the listed records
+            'ajax'      => false        //does this table support ajax?
+        ) );
+        
+    }
+  
+    function column_default($item, $column_name){
+		return $item[$column_name];
+
+    }
+    
+
+    function column_id($item){
+        
+        //Build row actions
+        $actions = array(
+            'edit'      => sprintf('<a href="?page=%s&action=%s&id=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
+            'delete'    => sprintf('<a href="?page=%s&action=%s&id=%s&amp;updated=true">Delete</a>',$_REQUEST['page'],'delete',$item['id']),
+        );
+        
+        //Return the user contents
+        return sprintf('%1$s %2$s',
+            /*$1%s*/ $item['id'],
+            /*$2%s*/ $this->row_actions($actions)
+        );
+    }
+    
+    /** ************************************************************************
+     * REQUIRED if displaying checkboxes or using bulk actions! The 'cb' column
+     * is given special treatment when columns are processed. It ALWAYS needs to
+     * have it's own method.
+     * 
+     * @see WP_List_Table::::single_row_columns()
+     * @param array $item A singular item (one full row's worth of data)
+     * @return string Text to be placed inside the column <td> (movie title only)
+     **************************************************************************/
+    function column_cb($item){
+        return sprintf(
+            '<input type="checkbox" name="%1$s[]" value="%2$s" />',
+            /*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("movie")
+            /*$2%s*/ $item['id']                //The value of the checkbox should be the record's id
+        );
+    }
+    
+    function get_columns(){
+        $columns = array(
+            'cb'        => '<input type="checkbox" />', //Render a checkbox instead of text
+			'id'     => 'Name',
+            'user'     => 'User',
+            'columns'    => 'Columns',
+			'bypage'    => 'Articles by page',
+			'featured'    => 'Featured articles',
+			'imgfeatured'    => 'Image'
+        );
+        return $columns;
+    }
+    
+    function get_sortable_columns() {
+        $sortable_columns = array(
+			'id'     => array('id',true),
+            'user'     => array('user',true),
+            'columns'    => array('columns',false),
+			'bypage'    => array('bypage',false),
+			'featured'    => array('featured',false),
+			'imgfeatured'    => array('imgfeatured',false)
+        );
+        return $sortable_columns;
+    }
+    
+    
+    /** ************************************************************************
+     * Optional. If you need to include bulk actions in your list table, this is
+     * the place to define them. Bulk actions are an associative array in the format
+     * 'slug'=>'Visible Title'
+     * 
+     * If this method returns an empty value, no bulk action will be rendered. If
+     * you specify any bulk actions, the bulk actions box will be rendered with
+     * the table automatically on display().
+     * 
+     * Also note that list tables are not automatically wrapped in <form> elements,
+     * so you will need to create those manually in order for bulk actions to function.
+     * 
+     * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
+     **************************************************************************/
+    function get_bulk_actions() {
+        $actions = array(
+            //'delete'    => 'Delete'
+        );
+        return $actions;
+    }
+    
+    
+    /** ************************************************************************
+     * Optional. You can handle your bulk actions anywhere or anyhow you prefer.
+     * For this example package, we will handle it in the class to keep things
+     * clean and organized.
+     * 
+     * @see $this->prepare_items()
+     **************************************************************************/
+    function process_bulk_action() {
+        
+        //Detect when a bulk action is being triggered...
+        if( 'delete'===$this->current_action() ) {
+            //wp_die('Items deleted (or they would be if we had items to delete)!');
+        }
+        
+    }
+    
+    
+    /** ************************************************************************
+     * REQUIRED! This is where you prepare your data for display. This method will
+     * usually be used to query the database, sort and filter the data, and generally
+     * get it ready to be displayed. At a minimum, we should set $this->items and
+     * $this->set_pagination_args(), although the following properties and methods
+     * are frequently interacted with here...
+     * 
+     * @global WPDB $wpdb
+     * @uses $this->_column_headers
+     * @uses $this->items
+     * @uses $this->get_columns()
+     * @uses $this->get_sortable_columns()
+     * @uses $this->get_pagenum()
+     * @uses $this->set_pagination_args()
+     **************************************************************************/
+    function prepare_items() {
+        global $wpdb; //This is used only if making any database queries
+
+        /**
+         * First, lets decide how many records per page to show
+         */
+        $per_page = 10;
+        
+        
+        /**
+         * REQUIRED. Now we need to define our column headers. This includes a complete
+         * array of columns to be displayed (slugs & titles), a list of columns
+         * to keep hidden, and a list of columns that are sortable. Each of these
+         * can be defined in another method (as we've done here) before being
+         * used to build the value for our _column_headers property.
+         */
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = $this->get_sortable_columns();
+        
+        
+        /**
+         * REQUIRED. Finally, we build an array to be used by the class for column 
+         * headers. The $this->_column_headers property takes an array which contains
+         * 3 other arrays. One for all columns, one for hidden columns, and one
+         * for sortable columns.
+         */
+        $this->_column_headers = array($columns, $hidden, $sortable);
+        
+        
+        /**
+         * Optional. You can handle your bulk actions however you see fit. In this
+         * case, we'll handle them within our package just to keep things clean.
+         */
+        $this->process_bulk_action();
+        
+        
+        $data = $this->options;
+                
+        
+        /**
+         * This checks for sorting input and sorts the data in our array accordingly.
+         * 
+         * In a real-world situation involving a database, you would probably want 
+         * to handle sorting by passing the 'orderby' and 'order' values directly 
+         * to a custom query. The returned data will be pre-sorted, and this array
+         * sorting technique would be unnecessary.
+         */
+        function usort_reorder($a,$b){
+            $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'user'; //If no sort, default to title
+            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
+            $result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
+            return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
+        }
+        usort($data, 'usort_reorder');
+        
+        
+
+        $current_page = $this->get_pagenum();
+        $total_items = count($data);
+        $data = array_slice($data,(($current_page-1)*$per_page),$per_page);
+        $this->items = $data;
+        
+        
+        /**
+         * REQUIRED. We also have to register our pagination options & calculations.
+         */
+        $this->set_pagination_args( array(
+            'total_items' => $total_items,                  //WE have to calculate the total number of items
+            'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
+            'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
+        ) );
+    }
+    
+}
+
+
+function tt_render_list_page($options){
+    
+    //Create an instance of our package class...
+    $testListTable = new TT_MLInt_List_Table($options);
+    //Fetch, prepare, sort, and filter our data...
+    $testListTable->prepare_items();
+    
+    ?>
+    <div class="wrap">
+        
+        <div id="icon-users" class="icon32"><br/></div>
+        <h2>Catalog List</h2>
+        
+        <form id="movies-filter" method="get">
+		<p class="submit">
+			<input type="button" name="newcatalog" value="<?php _e('New Catalog', 'mercadolibre_integration') ?>" onclick="location.href='<?php echo sprintf('?page=%s&action=%s',$_REQUEST['page'],'new') ?>'" />
+		</p>
+            <!-- For plugins, we also need to ensure that the form posts back to our current page -->
+            <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+            <!-- Now we can render the completed list table -->
+            <?php $testListTable->display() ?>
+		<p class="submit">
+			<input type="button" name="newcatalog" value="<?php _e('New Catalog', 'mercadolibre_integration') ?>" onclick="location.href='<?php echo sprintf('?page=%s&action=%s',$_REQUEST['page'],'new') ?>'" />
+		</p>
+        </form>
+		
+        
+    </div>
+<?php
+}
+?>
